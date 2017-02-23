e4a284d348ade24c077e4248647cd98720b6e127
ナノ <Nanomotion@users.noreply.github.com>
2017-02-23 18:00:59 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
-// document.location = "https://nanomotion.github.io/assets/html/errors/503.html";
+function getParameterByName(name, url) {
+    if (!url) {
+      url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+var x = getParameterByName("a");
+
+if (!(x == "1")) {
+  document.location = "https://nanomotion.github.io/assets/html/errors/coming-soon.html";
+}
