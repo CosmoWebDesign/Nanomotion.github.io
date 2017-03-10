94f5dceaf9607546d42d61ad14b2ab00b3516f59
ナノ <Nanomotion@users.noreply.github.com>
2017-03-10 13:45:52 -0500

    Create linkhandler.js

---
+++ assets/js/linkhandler.js
@@ -0,0 +1,21 @@
+function getParameterByName(name, url) {
+    if (!url) {
+      url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+var fwlink = getParameterByName(a);
+
+if (fwlink == "1") {
+  document.location = "http://help.nanomotion.xyz/topics/software/nano-os";
+} else if (fwlink == "2") {
+  document.location = "https://github.com/Nanomotion/Nano-OS/issues";
+} else {
+  document.location = "http://url.nanomotion.xyz/invalid-url.html";
+}
